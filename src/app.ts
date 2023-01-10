914dfc020f8f56f4926a7c051d99476770dfc8c2
saher <msaher.shair@gmail.com>
2023-01-10 17:58:18 +0300

    switch to ts imports

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
-import express from "express"
+import path from 'path';
+import fs from 'fs';
+import pandoc from 'node-pandoc'
+import express from "express";
+import Response from "express";
+
+
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+const basedir = "../";
+
 app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/*', (req, res) => {
+    console.log(req.params)
+    // const basename = req.params;
+    // const filename = path.join(basedir, basename);
+    // console.log(filename);
     res.render('index');
 });
 
